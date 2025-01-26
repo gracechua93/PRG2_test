59cd0df0a53597e057a72a756e8e808e9f30d02e
gracechua93 <chua.faithboy@gmail.com>
2025-01-26 19:25:12 +0800

    edit flight.cs

--- PRG2_test/PRG2_test/Flight.cs
+++ PRG2_test/PRG2_test/Flight.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace PRG2_test
 {
-    internal class Flight
+    class Flight
     {
+        // properties
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+
+        // constructors
+        public Flight() { }
+
+        public Flight(string fN, string o, string d, DateTime et)
+        {
+            FlightNumber = fN;
+            Origin = o;
+            Destination = d;
+            ExpectedTime = et;
+        }
+
+        public virtual double CalculateFees()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "Flight Number: " + FlightNumber +
+                "\nOrigin: " + Origin + "\nDestination: " +
+                Destination + "\nExpected Time: " + ExpectedTime + "\nStatus: " + Status;
+        }
+
     }
 }
